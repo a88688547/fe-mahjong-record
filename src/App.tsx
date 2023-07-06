77bfcd4356de9d136e63abe8e21ace0eaf80e2c2
Owen <owen@OwendeMacBook-Pro.local>
2023-07-06 21:09:49 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-// import './App.css';
-
 // reset CSS
 import "./style/reset.css";
 import {
   Route,
-  NavLink,
-  HashRouter,
-  BrowserRouter,
+  BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
 import { HomePage, TestPage } from "./pages";
@@ -16,26 +10,15 @@ import { HomePage, TestPage } from "./pages";
 function App() {
   return (
     <div className="App h-[100vh]">
-      {/* <div className="h-[100px] border-2 border-solid border-[green]">header</div>
-      <div className="h-[calc(100vh-200px)] relative"> */}
-
-        <BrowserRouter>
+      <div className="text-[red]">123123</div>
+        <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/test" element={<TestPage/>}/>
-              {/* <Route path="/Album" component={Album}/> */}
           </Routes>
-        </BrowserRouter>
-      {/* </div> */}
+        </Router>
     </div>
   );
 }
 
 export default App;
-
-// import AppRoutes from './routes';
-
-// const App = () => {
-//   return <AppRoutes />;
-// };
-// export default App;
